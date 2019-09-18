b3eb7acb7b7806005161167ca039f333182f3e37
Tish Chungoora <tishal82@gmail.com>
2019-09-18 14:59:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,10 +3,13 @@ def perform_calculations(source_array)
 end
 
 # Make all values negative
-source_array.map { |i| i * -1 }
+source_array.map{ |i| i * -1 }
 
 # Conserve the same values
-source_array.map { |i| i }
+source_array.map{ |i| i }
 
-# 
-source_array.map
+# Double all values
+source_array.map{ |i| i * 2 }
+
+# Square all values
+source_array.map{ |i| i ** 2}
