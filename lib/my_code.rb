7086157529fc32fc8cbd329310a5d5341d952257
Tish Chungoora <tishal82@gmail.com>
2019-09-18 14:58:15 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,8 +2,11 @@ def perform_calculations(source_array)
   yield(#{source_array})
 end
 
+# Make all values negative
 source_array.map { |i| i * -1 }
 
+# Conserve the same values
 source_array.map { |i| i }
 
+# 
 source_array.map
