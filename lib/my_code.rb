39cdc7635d646bbe0dafe18839b45047179f1a03
Tish Chungoora <tishal82@gmail.com>
2019-09-18 15:01:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,7 +9,7 @@ perform_transformations.map{ |i| i * -1 }
 perform_transformations.map{ |i| i }
 
 # Double all values
-perform_calculations.map{ |i| i * 2 }
+perform_transformations.map{ |i| i * 2 }
 
 # Square all values
-source_array.map{ |i| i ** 2 }
+perform_transformations.map{ |i| i ** 2 }
