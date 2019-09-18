3635529c00b5dc439949c52c02072d7abd021244
Tish Chungoora <tishal82@gmail.com>
2019-09-18 15:00:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,15 +1,15 @@
-def perform_calculations(source_array)
+def perform_transformations(source_array)
   yield(#{source_array})
 end
 
 # Make all values negative
-source_array.map{ |i| i * -1 }
+perform_transformations.map{ |i| i * -1 }
 
 # Conserve the same values
-source_array.map{ |i| i }
+perform_transformations.map{ |i| i }
 
 # Double all values
-source_array.map{ |i| i * 2 }
+perform_calculations.map{ |i| i * 2 }
 
 # Square all values
 source_array.map{ |i| i ** 2 }
